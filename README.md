cd250918cce960048bf43e155b84cee4cbd34475
fargly <kurt.danielson@gmail.com>
2025-07-23 13:07:25 -0500

    Bump

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bootstrap
 Minimal Bootstraping Sequence using Webi/Uvx/Ansible
+
+```bash
+# On macOS and Linux.
+curl -LsSf https://astral.sh/uv/install.sh | sh
+```
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,13 +7,18 @@ curl https://webi.sh | sh
 source ~/.config/envman/PATH.env
 
 # Webi Installs ############################################################ #
-for INSTALL in fd ripgrep bat xsv pathman fzf jq yq nerdfonts ;do
+for INSTALL in fd ripgrep bat xsv pathman fzf jq yq nerdfont ;do
     webi $INSTALL
 done
 
 # 'uv' Install ############################################################# #
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
+# Clone 'bootstrap' Repository ############################################# #
+(cd $HOME/.local/opt && git clone https://github.com/fargly/bootstrap.git)
+
+# Run Ansible Bootstrap Playbook ########################################### #
+uvx --from ansible-core ansible-playbook --help
 
 
 ## EOF
