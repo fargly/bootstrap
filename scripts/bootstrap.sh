2ff4bcec512c793d17704813e36949785b59dc5c
fargly <kurt.danielson@gmail.com>
2025-07-24 09:31:32 -0500

    Update bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,29 +1,19 @@
 #!/usr/bin/env bash
 
-# Install Webi ############################################################# #
-#curl https://webi.sh | sh
+# Minimal Bootstrapping Sequence To Launch Ansible Playbook ################ #
 
-# Source Environment ####################################################### #
-#. ~/.config/envman/PATH.env   ## Using source failed in this environment
-
-# Webi Installs ############################################################ #
-#for INSTALL in fd ripgrep bat xsv pathman fzf jq yq nerdfont ;do
-#for INSTALL in fd ripgrep bat fzf;do
-#    webi $INSTALL
-#done
-
-# Add ~/.local/bin Directory
+# Add ~/.local/bin Directory ############################################### #
 mkdir -p $HOME/.local/bin
-mkdir -p $HOME/.local/opt
 
 # 'uv' Install ############################################################# #
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # Clone 'bootstrap' Repository ############################################# #
 # curl https://api.github.com/users/fargly/keys | jq blahblah >> $HOME/.ssh/authorized_keys
-(cd $HOME/.local/opt && git clone https://github.com/fargly/bootstrap.git || (cd bootstrap && git pull))
+#(cd $HOME/.local/opt && git clone https://github.com/fargly/bootstrap.git || (cd bootstrap && git pull))
 
 # Run Ansible Bootstrap Playbook ########################################### #
+uvx --from ansible-core ansible-playbook $HOME/.local/opt/bootstrap/ansible/bootstrap.yaml 
 uvx --from ansible-core ansible-playbook $HOME/.local/opt/bootstrap/ansible/bootstrap.yaml 
 
 
