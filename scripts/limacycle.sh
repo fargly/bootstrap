d7e8094372f60cc90287b6c0625b5d445e30f75f
fargly <kurt.danielson@gmail.com>
2025-08-08 08:14:11 -0500

    Add vm recycle helper script

---
+++ scripts/limacycle.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <lima vm name>"
+  exit 1
+fi
+
+LIMA_VM_NAME="$1"
+limactl stop "$LIMA_VM_NAME"
+limactl delete "$LIMA_VM_NAME"
+limactl create "$LIMA_VM_NAME"
+limactl start "$LIMA_VM_NAME"
+
+
+## EOF
