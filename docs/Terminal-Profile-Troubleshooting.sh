f8599c2b716784f4eb2aa566c4159a13b02a83c9
fargly <kurt.danielson@gmail.com>
2025-08-29 12:32:04 -0500

    Add terminal profile doc

---
+++ docs/Terminal-Profile-Troubleshooting.sh
@@ -0,0 +1,59 @@
+
+gsettings get org.gnome.Terminal.ProfilesList list
+
+gsettings list-recursively "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/"
+
+['b1dcc9dd-5262-4d8d-a863-c897e6d979b9']
+
+org.gnome.Terminal.Legacy.Profile font 'Monospace 12'
+org.gnome.Terminal.Legacy.Profile use-system-font true
+
+
+
+org.gnome.Terminal.Legacy.Profile audible-bell true
+org.gnome.Terminal.Legacy.Profile background-color 'rgb(23,20,33)'
+org.gnome.Terminal.Legacy.Profile background-transparency-percent 50
+org.gnome.Terminal.Legacy.Profile backspace-binding 'ascii-delete'
+org.gnome.Terminal.Legacy.Profile bold-color '#000000'
+org.gnome.Terminal.Legacy.Profile bold-color-same-as-fg true
+org.gnome.Terminal.Legacy.Profile bold-is-bright false
+org.gnome.Terminal.Legacy.Profile cell-height-scale 1.0
+org.gnome.Terminal.Legacy.Profile cell-width-scale 1.0
+org.gnome.Terminal.Legacy.Profile cjk-utf8-ambiguous-width 'narrow'
+org.gnome.Terminal.Legacy.Profile cursor-background-color '#000000'
+org.gnome.Terminal.Legacy.Profile cursor-blink-mode 'system'
+org.gnome.Terminal.Legacy.Profile cursor-colors-set false
+org.gnome.Terminal.Legacy.Profile cursor-foreground-color '#ffffff'
+org.gnome.Terminal.Legacy.Profile cursor-shape 'block'
+org.gnome.Terminal.Legacy.Profile custom-command ''
+org.gnome.Terminal.Legacy.Profile default-size-columns 80
+org.gnome.Terminal.Legacy.Profile default-size-rows 24
+org.gnome.Terminal.Legacy.Profile delete-binding 'delete-sequence'
+org.gnome.Terminal.Legacy.Profile enable-bidi true
+org.gnome.Terminal.Legacy.Profile enable-shaping true
+org.gnome.Terminal.Legacy.Profile enable-sixel false
+org.gnome.Terminal.Legacy.Profile encoding 'UTF-8'
+org.gnome.Terminal.Legacy.Profile exit-action 'close'
+org.gnome.Terminal.Legacy.Profile font 'Monospace 12'
+org.gnome.Terminal.Legacy.Profile foreground-color 'rgb(208,207,204)'
+org.gnome.Terminal.Legacy.Profile highlight-background-color '#000000'
+org.gnome.Terminal.Legacy.Profile highlight-colors-set false
+org.gnome.Terminal.Legacy.Profile highlight-foreground-color '#ffffff'
+org.gnome.Terminal.Legacy.Profile login-shell false
+org.gnome.Terminal.Legacy.Profile palette ['#000000000000', '#EEEE00000000', '#4E4E9A9A0606', '#C4C4A0A00000', '#34346565A4A4', '#757550507B7B', '#060698989A9A', '#D3D3D7D7CFCF', '#555557575353', '#EFEF29292929', '#8A8AE2E23434', '#FCFCE9E94F4F', '#72729F9FCFCF', '#ADADB7B72D2D', '#3434E2E2E2E2', '#FCFCFCECECEC']
+org.gnome.Terminal.Legacy.Profile preserve-working-directory 'safe'
+org.gnome.Terminal.Legacy.Profile rewrap-on-resize true
+org.gnome.Terminal.Legacy.Profile scroll-on-insert true
+org.gnome.Terminal.Legacy.Profile scroll-on-keystroke true
+org.gnome.Terminal.Legacy.Profile scroll-on-output false
+org.gnome.Terminal.Legacy.Profile scrollback-lines 10000
+org.gnome.Terminal.Legacy.Profile scrollback-unlimited false
+org.gnome.Terminal.Legacy.Profile scrollbar-policy 'always'
+org.gnome.Terminal.Legacy.Profile text-blink-mode 'always'
+org.gnome.Terminal.Legacy.Profile use-custom-command false
+org.gnome.Terminal.Legacy.Profile use-system-font true
+org.gnome.Terminal.Legacy.Profile use-theme-colors false
+org.gnome.Terminal.Legacy.Profile use-theme-transparency true
+org.gnome.Terminal.Legacy.Profile use-transparent-background false
+org.gnome.Terminal.Legacy.Profile visible-name 'Unnamed'
+org.gnome.Terminal.Legacy.Profile word-char-exceptions @ms nothing
